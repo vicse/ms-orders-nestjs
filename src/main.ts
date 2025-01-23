fa69c376b2fced20a77a43bee5f4ba231ef0fa1e
vicse <vicseore@gmail.com>
2025-01-23 11:13:29 -0500

    feat: orders CRUD microservice without logic

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './common/config';
 import { Logger } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
-  const app = await NestFactory.create(AppModule);
-  await app.listen(envs.port);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: { port: envs.port },
+    },
+  );
+  await app.listen();
   logger.log(`Orders ms running on port ${envs.port}`);
 }
 bootstrap();
