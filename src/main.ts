3cd29a898cc959f9483cfcd4b601db17225f4d62
vicse <vicseore@gmail.com>
2025-01-22 23:22:13 -0500

    feat: config envs added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './common/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(envs.port);
+  logger.log(`Orders ms running on port ${envs.port}`);
 }
 bootstrap();
